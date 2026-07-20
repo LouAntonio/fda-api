7b6a2aa582c91fe54262d8df4112aabb347984d3
LouAntonio <lourencodala88@gmail.com>
2026-07-20 23:06:42 +0100

    refactor: simplify adapter creation in Prisma client initialization

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -3,9 +3,7 @@ import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 
 function createExtendedClient() {
-	const adapter = new PrismaPg({
-		connectionString: process.env.DATABASE_URL!,
-	});
+	const adapter = new PrismaPg(process.env.DATABASE_URL!);
 
 	return new PrismaClient({ adapter });
 }
